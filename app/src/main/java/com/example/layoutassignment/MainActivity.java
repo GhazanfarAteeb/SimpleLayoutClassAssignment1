27896783d32c04cd3b39025128c597b0eade4614
Ghazanfar Ateeb <strange.person9125@gmail.com>
2021-09-09 23:02:05 +0500

    Created Question 1's 2 layouts with Linear Layout & Initial Commit

---
+++ app/src/main/java/com/example/layoutassignment/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.layoutassignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.assignment_1_question_1_linear_layout_2);
+    }
+}
